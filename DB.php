7239d52d6ce220d088b3be2dab353f72ef19c4a7
Dev11 <mohammadmghig@gmail.com>
2025-11-05 14:50:13 +0330

    run job with id

--- DB.php
+++ DB.php
@@ -9,4 +9,20 @@ class DB
         $stmt = $this->pdo->query('SELECT COUNT(*) FROM jobs');
         return $stmt->fetchColumn();
     }
+
+    public function get($id) {
+        $stmt = $this->pdo->prepare(
+            "SELECT * FROM jobs WHERE id = ?"
+        );
+
+        $stmt->execute([$id]);
+
+        $job = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($job) {
+            return $job;
+        }
+
+        return null;
+    }
 }
--- Worker.php
+++ Worker.php
@@ -13,12 +13,35 @@ $countExisted = $db->count();
 
 $args = parseCommandArgs($argv);
 $retry     = $args['retry']     ?? 0;
+$id        = $args['id']     ?? null;
 $runEvery  = $args['run-every'] ?? 1;
 $queueName = $args['queue-name'] ?? 'default';
 $stopWhenEmpty = $args['stop-when-empty'] ?? false;
  
  
+// run job with id
+if ($retry && $id) {
+    $jobData = $db->get($id);
+    if ($jobData) {
+        $job = unserialize(base64_decode($jobData['payload']));
+        try {
+            $job->handle();
+            $queue->delete($jobData['id']);
+            echo "Processed job #{$jobData['id']}\n";
+        } catch (Exception $e) {
+            $queue->markJobAsFailed($jobData['id']);
+        }
+    } else {
+        print("Job not found!");
+    } 
+    return;
+}
 
+
+/**
+ * This is one of main part of application it run a infinit loop
+ * to get jobs from storage(db,redis,...) and run it 
+ */
 while (true) {
     if ($stopWhenEmpty && $countExisted == 0) {
         break;
@@ -39,7 +62,6 @@ while (true) {
         } catch (Exception $e) {
             $queue->markJobAsFailed($jobData['id']);
         }
-    }
-
+    } 
     sleep($runEvery ?? 1);
 }
